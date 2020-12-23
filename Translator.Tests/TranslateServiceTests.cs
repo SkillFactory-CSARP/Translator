8fadd87054d932b951b2f1131a2e82a40b39eed4
GitBrainsStudio <gitbrains@gmail.com>
2020-12-23 22:00:53 +0500

    initial commit

---
+++ Translator.Tests/TranslateServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Translator.Tests
+{
+    public class TranslateServiceTests
+    {
+        [Fact]
+        public void GetTranslationMustBeReturnCorrectValue()
+        {
+            var translateService = new TranslateService();
+            translateService.AddTranslation("orange", "апельсин");
+
+            Assert.Equal("апельсин", translateService.GetTranslation("orange"));
+        }
+    }
+}
---
+++ Translator/TranslateService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translator
+{
+    public class TranslateService
+    {
+        public Dictionary<string, string> _translation;
+
+        public TranslateService()
+        {
+            _translation = new Dictionary<string, string>();
+            _translation.Add("dog", "собака");
+            _translation.Add("cat", "кошка");
+        }
+
+        public void AddTranslation(string oneWord, string secondWord)
+        {
+            _translation.Add(oneWord, secondWord);
+        }
+
+        public string GetTranslation(string word)
+        {
+            return _translation[word];
+        }
+    }
+}
